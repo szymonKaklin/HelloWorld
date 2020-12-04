5c27379ba6f6783fd326e79bbce46d949a384e6e
Szymon Kaklin <szymon.kaklin@hotmail.co.uk>
2020-12-04 17:51:23 +0000

    Added Button Wrappers

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -8,9 +8,23 @@
 
 #include "MainComponent.h"
 
+template<class ButtonType>
+auto makeButton(ButtonType* buttonToMakeUnique) -> std::unique_ptr<ButtonType>
+{
+    return std::unique_ptr<ButtonType>(buttonToMakeUnique);
+}
+
 //==============================================================================
 MainComponent::MainComponent()
 {
+    heapButton.reset(new HeapButtonWrapper<TextButton>([](){ DBG("you clicker the heap" );},
+                                               new TextButton("heapButton")
+                                                       )
+                     );
+    addAndMakeVisible(button);
+    
+    addAndMakeVisible( (*heapButton) );
+    
     setSize (600, 400);
 }
 
@@ -34,4 +48,11 @@ void MainComponent::resized()
     // This is called when the MainComponent is resized.
     // If you add any child components, this is where you should
     // update their positions.
+    
+    button->setBounds(500,
+                      0,
+                      100, 30);
+    (*heapButton)->setBounds(500,
+                             368,
+                             100, 30);
 }
--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -10,27 +10,88 @@
 
 #include "../JuceLibraryCode/JuceHeader.h"
 
-struct Car
-{
-  struct CarSeat
-    {
-        bool seatIsLeather;
-    };
-    
-    int numberOfWheels;
-    bool convertible;
-    CarSeat driversSeat;
-    CarSeat navigatorsSeat;
-    
-    bool switchSeats( CarSeat oldSeat, CarSeat newSeat );
-    
-    void accelerate(float howFarToPushThePedal);
-};
 //==============================================================================
 /*
     This component lives inside our window, and this is where you should put all
     your controls and content.
 */
+template<typename ButtonType>
+struct ButtonWrapper : public Button::Listener
+{
+    template<typename ... Args>
+    ButtonWrapper( std::function<void()> onClickHandler,
+                  Args&& ... args ) :
+    onClick( std::move( onClickHandler ) ),
+    button( std::forward<Args>(args) ... )
+    {
+        button.addListener(this);
+    }
+    ~ButtonWrapper() { button.removeListener(this); }
+    void buttonClicked(Button* b) override
+    {
+        if( onClick)
+            onClick();
+    }
+    //operator Component*() { return &button; }
+    operator Component&() { return button; }
+    Button* operator->() { return &button; }
+private:
+    std::function<void()> onClick;
+    ButtonType button;
+};
+
+
+
+template<typename ButtonType>
+struct HeapButtonWrapper : public Button::Listener
+{
+    HeapButtonWrapper(std::function<void()> onClickHandler,
+                      std::unique_ptr<ButtonType> heapButton) :
+    onClick( std::move( onClickHandler ) ),
+    button( std::move(heapButton) )
+    {
+        setupButton();
+    }
+
+    HeapButtonWrapper(std::function<void()> onClickHandler,
+                      ButtonType* rawButtonPtr) :
+    HeapButtonWrapper(onClickHandler,
+                      std::unique_ptr<ButtonType>(rawButtonPtr) )
+    {
+        
+    }
+    
+    ~HeapButtonWrapper()
+    {
+        if( button.get() )
+            button->removeListener(this);
+    }
+    void buttonClicked(Button* b) override
+    {
+        if( onClick)
+            onClick();
+    }
+    
+    operator Component*()
+    {
+        if( button.get() != nullptr )
+            return button.get();
+            return nullptr;
+    }
+    Button* operator->() { return button.get(); }
+private:
+    void setupButton()
+    {
+        jassert( button.get() != nullptr );
+        if( button.get() )
+            button->addListener(this);
+    }
+    std::function<void()> onClick;
+    std::unique_ptr<ButtonType> button;
+};
+
+
+
 class MainComponent   : public Component
 {
 public:
@@ -43,9 +104,86 @@ public:
     void resized() override;
 
 private:
+    ButtonWrapper<TextButton> button {[]() { DBG( "you clicked the button" ); }, "textB" };
+    std::unique_ptr<HeapButtonWrapper<TextButton>> heapButton;
     //==============================================================================
     // Your private member variables go here...
 
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
 };
+
+
+/*
+ INHERITANCE
+ a cat IS A vehicle
+ a car IS A modern invention
+ a car IS A status symbol
+ a car IS A Tool
+ 
+ COMPOSITION
+ a car HAS A heater
+ a car HAS A engine
+ a car HAS A air conditioner
+ a car HAS A battery
+ */
+
+//struct Tool
+//{
+//    Tool( const String& purpose_ = "to perform work" ) : purpose(purpose_) { }
+//    String purpose;
+//};
+//
+//struct TemperatureController { int currentTemp = 0; };
+//
+//struct PowerSupply
+//{
+//    PowerSupply(int po) : powerOutput(po) { }
+//    int powerOutput = 0;
+//};
+//
+//struct Heater : public TemperatureController { };
+//
+//struct Engine : public PowerSupply
+//{
+//    Engine( int power = 5 ) : PowerSupply( power ) { }
+//};
+//
+//struct Battery : public PowerSupply
+//{
+//    Battery( int power = 3 ) : PowerSupply( power ) { }
+//};
+//struct AirConditioner : public TemperatureController { };
+//
+//struct Vehicle : public Tool
+//{
+//    Vehicle( const String& purpose_, int enginePower, int batteryPower) :
+//    Tool(purpose_),
+//    engine(enginePower),
+//    battery(batteryPower)
+//    { }
+//    Heater heater;
+//    AirConditioner airConditioner;
+//    Engine engine;
+//    Battery battery;
+//};
+//struct ModernInvention
+//{
+//    int yearInvented = 1901;
+//};
+//
+//struct StatusSymbol {};
+//
+//struct SemiTruck : public Vehicle
+//{
+//    SemiTruck () : Vehicle("to move cargo", 20, 10) { }
+//};
+//
+//struct Car : public Vehicle, public ModernInvention, public StatusSymbol
+//{
+//    Car() : Vehicle("to move people", 5, 2)
+//    {
+//        yearInvented = 1808;
+//        this->ModernInvention::yearInvented = 1808;
+//    }
+//};
